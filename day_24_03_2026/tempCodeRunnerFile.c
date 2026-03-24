498a18d424f6efd46b22e895b423361752bc56e9
PetarBuhchev <petarbuhchev@outlook.com>
2026-03-24 17:13:35 +0200

    task3.c

---
+++ day_24_03_2026/tempCodeRunnerFile.c
@@ -0,0 +1,23 @@
+//Напишете програма, която получава масив от N елемента и обръща реда на елементите. Изкарайте ново полученият масив на екрана.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    // Reverse the array
+    for (int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+    // Print the reversed array
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
